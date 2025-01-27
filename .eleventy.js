19f2c363dd37a237b0effba1c6e79d064589446a
Csaba Boros <boros.csaba94@gmail.com>
2025-01-27 11:32:04 +0100

    Initial code

---
+++ .eleventy.js
@@ -0,0 +1,92 @@
+const Image = require('@11ty/eleventy-img');
+const outdent = require('outdent');
+const htmlmin = require('html-minifier');
+
+const imageShortcode = async (
+  src,
+  alt,
+  className = undefined,
+  widths = [400, 800, 1280],
+  formats = ['webp', 'png'],
+  sizes = '100vw'
+) => {
+  const imageMetadata = await Image(src, {
+    widths: [...widths],
+    formats: [...formats],
+    outputDir: './dist/assets/img/',
+    urlPath: '/assets/img/',
+    sharpOptions: {
+      animated: true
+    }
+  });
+
+  const sourceHtmlString = Object.values(imageMetadata)
+    .map((images) => {
+      const { sourceType } = images[0];
+      const sourceAttributes = stringifyAttributes({
+        type: sourceType,
+        srcset: images.map((image) => image.srcset).join(', '),
+        sizes,
+      });
+
+      return `<source ${sourceAttributes}>`;
+    })
+    .join('\n');
+
+  const getLargestImage = (format) => {
+    const images = imageMetadata[format];
+    return images[images.length - 1];
+  }
+
+  const largestUnoptimizedImg = getLargestImage(formats[0]);
+  const imgAttributes = stringifyAttributes({
+    src: largestUnoptimizedImg.url,
+    width: largestUnoptimizedImg.width,
+    height: largestUnoptimizedImg.height,
+    alt,
+    loading: 'lazy',
+    decoding: 'async'
+  });
+  const imgHtmlString = `<img ${imgAttributes}>`;
+
+  const pictureAttributes = stringifyAttributes({
+    class: className,
+  });
+  const picture = `<picture ${pictureAttributes}>
+    ${sourceHtmlString}
+    ${imgHtmlString}
+  </picture>`;
+
+  return outdent`${picture}`;
+};
+
+const stringifyAttributes = (attributeMap) => {
+    return Object.entries(attributeMap)
+      .map(([attribute, value]) => {
+        if (typeof value === 'undefined') return '';
+        return `${attribute}="${value}"`;
+      })
+      .join(' ');
+  };
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addShortcode('image', imageShortcode);
+    eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
+        if (outputPath.endsWith('.html')) {
+            return htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+        }
+        return content;
+    });
+
+    return {
+        dir: {
+            input: "src",
+            output: "dist",
+        }
+    }
+}
